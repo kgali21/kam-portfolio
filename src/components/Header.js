a0bd22d944012491f7df77dd010ad5e11658ac7f
Kamran Ali <kgali21@gmail.com>
2019-11-27 10:32:18 -0800

    created header component

---
+++ src/components/Header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Header = () => (
+  <>
+    <header>
+      <h1>Kamran Ali</h1>
+      <h2>Full-Stack Software Developer</h2>
+      <nav>
+        <p>Home</p>
+        <p>About Me</p>
+        <p>Portfolio</p>
+        <p>Contact Us</p>
+      </nav>
+    </header>
+  </>
+);
+
+export default Header;
