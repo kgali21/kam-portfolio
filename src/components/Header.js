7b8935ccf01164addad1e1949a7c4f7dd5e1ba3d
Kamran Ali <kgali21@gmail.com>
2020-03-16 14:36:22 -0700

    flip flopped colors, SideNavBar and Header are set

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,12 +6,6 @@ const Header = () => (
     <header className={styles.Header}>
       <h1 className={styles.Name}>Kamran Ali</h1>
       <h2 className={styles.Role}>Full-Stack Software Developer</h2>
-      {/* <nav className={styles.Nav}>
-        <p>Home</p>
-        <p>About Me</p>
-        <p>Portfolio</p>
-        <p>Contact Me</p>
-      </nav> */}
     </header>
   </>
 );
