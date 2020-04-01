eb3567296b6b28f59533ed3b7c837b1ecd8e6a73
Kamran <kgali21@gmail.com>
2020-04-01 14:29:41 -0700

    added proptypes to projects component

--- src/components/projects/ProjectCard.js
+++ src/components/projects/ProjectCard.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+
 const Project = ({ project, img, description, frontendTech, backendTech }) => (
     <div>
         <h1>{project}</h1>
@@ -8,5 +10,13 @@ const Project = ({ project, img, description, frontendTech, backendTech }) => (
         <p>{backendTech}</p>
     </div>
 )
+
+Project.propTypes = {
+    project: PropTypes.string,
+    img: PropTypes.string,
+    description: PropTypes.string,
+    frontendTech: PropTypes.string,
+    backendTech: PropTypes.string
+}
 
 export default Project;
