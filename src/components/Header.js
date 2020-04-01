986e6c66c8dcc0214273cbe4546af290a41521df
Kamran <kgali21@gmail.com>
2020-04-01 13:27:06 -0700

    styling nav bar with hover and color; fixed header to be more coding related; switched header color

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,8 +4,8 @@ import styles from './Header.css';
 const Header = () => (
   <>
     <header className={styles.Header}>
-      <h1 className={styles.Name}>Kamran Ali</h1>
-      <h2 className={styles.Role}>Full-Stack Software Developer</h2>
+      <h1 className={styles.Name}>const SD = Kamran Ali</h1>
+      <h2 className={styles.Role}>expect(Full-Stack Software Developer).toBe(SD)</h2>
     </header>
   </>
 );
