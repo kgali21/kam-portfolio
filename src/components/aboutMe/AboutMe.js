d5c446f9ac5d4acc0415834072ff77a390042d2d
Kamran <kgali21@gmail.com>
2020-04-02 14:33:46 -0700

    decided to split site into 3 main parts; started about me contact info

---
+++ src/components/aboutMe/AboutMe.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const AboutMe = () => (
+    <>
+        <h1>About Me:</h1>
+        <h2>Contact me: </h2>
+            <div>
+                <p>Phone: (503)-789-0553</p>
+                <p>Email: Kgali21@gmail.com</p>
+                <p></p>
+                <p></p>
+            </div>
+    </>
+)
