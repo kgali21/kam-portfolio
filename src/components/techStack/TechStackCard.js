271c0c43de268c315edd70d81e3df46bc4670bef
Kamran <kgali21@gmail.com>
2020-04-03 11:15:18 -0700

    tech stack is scaffolded

---
+++ src/components/techStack/TechStackCard.js
@@ -0,0 +1,50 @@
+import React from 'react';
+
+
+const TechStackCard = () => (
+    <>
+        <div>
+            <div>
+                <p>LANGUAGES</p>
+                <p>Javascript</p>
+                <p>HTML</p>
+                <p>CSS</p>
+            </div>
+            <div>
+                <p>FRONT-END</p>
+                <p>React and React Hooks</p>
+                <p>Redux</p>
+            </div>
+            <div>
+                <p>BACK-END</p>
+                <p>Node.js</p>
+                <p>Express</p>
+                <p>Mongoose</p>
+            </div>
+            <div>
+                <p>TOOLS</p>
+                <p>Github</p>
+                <p>Heroku</p>
+                <p>VScode</p>
+                <p>Postman</p>
+                <p>P5.js</p>
+                <p>Socket.io</p>
+                <p>Pgadmin</p>
+                <p>Photoshop</p>
+            </div>
+            <div>
+                <p>DATABASES</p>
+                <p>MongoDB</p>
+                <p>PostgresQL</p>
+            </div>
+            <div>
+                <p>TESTING</p>
+                <p>Jest</p>
+                <p>QUnit</p>
+                <p></p>
+            </div>
+        </div>
+    </>
+)
+
+export default TechStackCard;
