8c23f39354dd99d714f711f462074a9fdee76125
Kamran <kgali21@gmail.com>
2020-04-03 11:55:13 -0700

    lined up tech stack

--- src/components/techStack/TechStackCard.js
+++ src/components/techStack/TechStackCard.js
@@ -8,42 +8,50 @@ const TechStackCard = () => (
         <div className={styles.TechStack}>
             <div>
                 <p>LANGUAGES</p>
-                <p>Javascript</p>
-                <p>HTML</p>
-                <p>CSS</p>
+                    <div className={styles.Lang}>
+                        <p>Javascript</p>
+                        <p>HTML</p>
+                        <p>CSS</p>
+                    </div>
             </div>
             <div>
                 <p>FRONT-END</p>
-                <p>React and React Hooks</p>
-                <p>Redux</p>
+                    <div className={styles.FrontEnd}>
+                        <p>React and React Hooks</p>
+                        <p>Redux</p>
+                    </div>
             </div>
             <div>
                 <p>BACK-END</p>
-                <p>Node.js</p>
-                <p>Express</p>
-                <p>Mongoose</p>
+                <div className={styles.BackEnd}>
+                    <p>Node.js</p>
+                    <p>Express</p>
+                    <p>Mongoose</p>
+                </div>
             </div>
             <div>
                 <p>TOOLS</p>
-                <p>Github</p>
-                <p>Heroku</p>
-                <p>VScode</p>
-                <p>Postman</p>
-                <p>P5.js</p>
-                <p>Socket.io</p>
-                <p>Pgadmin</p>
-                <p>Photoshop</p>
+                <div className={styles.Tools}>
+                    <p>Github</p>
+                    <p>Heroku</p>
+                    <p>VScode</p>
+                    <p>Postman</p>
+                    <p>Socket.io</p>
+                </div>
             </div>
             <div>
                 <p>DATABASES</p>
-                <p>MongoDB</p>
-                <p>PostgresQL</p>
+                <div className={styles.Database}>
+                    <p>MongoDB</p>
+                    <p>PostgresQL</p>
+                </div>
             </div>
             <div>
                 <p>TESTING</p>
-                <p>Jest</p>
-                <p>QUnit</p>
-                <p></p>
+                <div className={styles.Testing}>
+                    <p>Jest</p>
+                    <p>QUnit</p>
+                </div>
             </div>
         </div>
     </>
