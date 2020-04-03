19924eb40bcdec59d93660c0abaf42e1d7a08de3
Kamran <kgali21@gmail.com>
2020-04-03 11:04:57 -0700

    scaffolding tech stack section\

---
+++ src/components/techStack/TechStack.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const TechStack = () => (
+    <>
+        <div>
+            <p>LANGUAGES</p>
+            <p>Javascript</p>
+            <p>HTML</p>
+            <p>CSS</p>
+        </div>
+        <div>
+            <p></p>
+            <p></p>
+            <p></p>
+            <p></p>
+        </div>
+        <div>
+            <p></p>
+            <p></p>
+            <p></p>
+            <p></p>
+        </div>
+    </>
+)
