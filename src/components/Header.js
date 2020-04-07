e0d75a819715f51d0d47dc32386919c4e8539bfe
Kamran <kgali21@gmail.com>
2020-04-06 17:17:20 -0700

    fixed kam header title

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,9 +3,11 @@ import styles from './Header.css';
 import SideBarNav from './SideBarNav';
 const Header = () => (
   <>
-    <header className={styles.Header}>
-    <h1 className={styles.Name}>const SD = Kamran Ali</h1>
-    <h2 className={styles.Role}>expect(Full-Stack Software Developer).toBe(SD)</h2>
+    <header className={styles.HeaderComponent}>
+      <div className={styles.Headers}>
+        <h1 className={styles.Name}>Kamran Ali</h1>
+        <h2 className={styles.Role}>Full-Stack Software Developer</h2>
+      </div>
     <SideBarNav />
     </header>
   </>
