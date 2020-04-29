847bba99a19ed335031c450903bc6af7e8da9330
Kamran <kgali21@gmail.com>
2020-04-29 13:50:38 -0700

    changed the size of the project cards; placed links to projects on card

--- src/components/projects/ProjectCard.js
+++ src/components/projects/ProjectCard.js
@@ -4,17 +4,19 @@ import PropTypes from 'prop-types';
 import styles from './ProjectCard.css';
 
 const Project = ({ project, image, description, frontendTech, backendTech, site, github }) => (
+    <>
     <div className={styles.ListItem}>
         <h1>{project}</h1>
         <img src={image}></img>
         <p>{description}</p>
         <p>Front-End Tech: {frontendTech}</p>
         <p>Back-End Tech: {backendTech}</p>
+        <div className={styles.Links}>
+            <a href={site}>Site</a>
+            <a href={github}>GitHub</a>
+        </div>
     </div>
-    <div>
-        <a href={site}>Site</a>
-        <a href={github}>GitHub</a>
-    </div>
+    </>
 )
 
 Project.propTypes = {
